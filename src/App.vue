d1ad5b6faef119bc61b8c6b5a196a947ba225ce3
bruno_yu <bruno_yu@wonder4stuidio.com>
2023-12-19 17:32:26 +0800

    docs: 調整文檔

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <script setup lang="ts">
-// import request from '@/utils/request'
 import { onMounted } from 'vue'
 import { reqLogin } from './api/user'
-// import SvgIcon from '@/components/SvgIcon/index.vue'
 
 onMounted(() => {
-  // request({
-  //   url: '/user/login',
-  //   method: 'post',
-  //   data: {
-  //     username: 'admin',
-  //     password: '123456',
-  //   },
-  // }).then((res) => {
-  //   console.log(res)
-  // })
   reqLogin({ username: 'admin', password: '123456' })
 })
 </script>
 
 <template>
   <div>
-    <h1>我是 APP 根組件</h1>
+    <h1 class="text-title">Vue starter 小專案模板</h1>
     <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.text-color {
+.text-title {
+  font-size: 3rem;
+  font-weight: bold;
   color: $color;
 }
 </style>
